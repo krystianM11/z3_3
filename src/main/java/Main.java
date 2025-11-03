77b3e339cfc5b067cd7894c4b7456caf2795abc3
krystianM11 <krystekq6u12@gmail.com>
2025-11-03 16:54:47 +0000

    z3_3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,28 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        int rozmiar = 5;
+    public class Main {
+        public static void main(String[] args) {
+            int rozmiar = 5;
 
-        
-        for (int i = 0; i < rozmiar; i++) {
-            for (int j = 0; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        System.out.println(); 
-
-        
-        for (int i = 0; i < rozmiar; i++) {
-            for (int j = 0; j <= i; j++) {
-                
-                if (j == 0 || j == i || i == rozmiar - 1) {
+            
+            for (int i = 0; i < rozmiar; i++) {
+                for (int j = 0; j <= i; j++) {
                     System.out.print("*");
-                } else {
-                    System.out.print(" ");
                 }
+                System.out.println();
             }
-            System.out.println();
+
+            System.out.println(); 
+
+            
+            for (int i = 0; i < rozmiar; i++) {
+                for (int j = 0; j <= i; j++) {
+                    if (j == 0 || j == i || i == rozmiar - 1) {
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+
+            System.out.println(); 
+
+            
+            for (int i = 0; i < rozmiar; i++) {
+                for (int j = 0; j < rozmiar - i - 1; j++) {
+                    System.out.print(" "); 
+                }
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
         }
     }
-}
