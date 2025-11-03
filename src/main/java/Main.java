418026b998e26285d015c13e99ab6828e91c627b
krystianM11 <krystekq6u12@gmail.com>
2025-11-03 16:49:18 +0000

    z3_3_2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        int rozmiar = 5; 
+        int rozmiar = 5;
 
+        
         for (int i = 0; i < rozmiar; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print("*");
             }
-            System.out.println(); 
+            System.out.println();
+        }
+
+        System.out.println(); 
+
+        
+        for (int i = 0; i < rozmiar; i++) {
+            for (int j = 0; j <= i; j++) {
+                
+                if (j == 0 || j == i || i == rozmiar - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
         }
     }
 }
